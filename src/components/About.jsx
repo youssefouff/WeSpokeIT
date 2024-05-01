ec73e249d337c71e8a90fc2877a29d62ade8052b
Youssef Ouf <47798816+youssefouff@users.noreply.github.com>
2024-05-01 19:42:12 +0300

    Update About Section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
         </span>
       </h2>
       <p className="mt-2 text-lg text-center text-neutral-400 max-w-6xl">
-       At WeSpokeIT, we specialize in providing automated reconciliation and banking services that streamline financial operations.
+       At WeSpokeIT, we specialize in providing automated tools and services that streamline financial operations.
        Our goal is to revolutionize the finance industry by introducing innovative tools and technologies. 
        With our advanced reconciliation platform, we empower businesses to optimize their processes and stay ahead in today's dynamic financial landscape. We are committed to driving efficiency, accuracy, and reliability in financial operations, ensuring our clients have the tools they need to succeed. Join us as we pave the way for the future of finance, one innovative solution at a time.
       </p>
